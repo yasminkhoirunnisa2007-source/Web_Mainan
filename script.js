02976a1f2ba80e22764213621c30c5c8eb2b37f4
abianHakim <abianhakim09@gmail.com>
2026-05-24 23:43:41 +0700

    membenarkan responsive mobile dsb

--- script.js
+++ script.js
@@ -2,12 +2,7 @@ const navToggle = document.querySelector(".nav-toggle");
 const siteNav = document.querySelector(".site-nav");
 
 navToggle.addEventListener("click", () => {
-  const isOpen = siteNav.classList.toggle("is-open");
-
-  navToggle.setAttribute(
-    "aria-expanded",
-    String(isOpen)
-  );
+  siteNav.classList.toggle("active");
 });
 
 siteNav.querySelectorAll("a").forEach((link) => {
